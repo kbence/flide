fffa0af6f507ffd840fa00d6428aea0d83f4ec89
Bence Kiglics <bence@kiglics.hu>
2014-01-07 06:41:42 +0100

    implemented window

--- src/flide.cpp
+++ src/flide.cpp
@@ -8,6 +8,43 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	SDL_Window *window = SDL_CreateWindow("Flide", 100, 100, 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
+	if (!window) {
+		fprintf(stderr, "SDL_CreateWindow failed!");
+		return 1;
+	}
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	if (!context) {
+		fprintf(stderr, "SDL_GL_CreateContext failed!");
+		return 1;
+	}
+
+	SDL_GL_MakeCurrent(window, context);
+
+	bool running = true;
+
+	while (running) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_WINDOWEVENT:
+					switch (event.window.event) {
+						case SDL_WINDOWEVENT_CLOSE:
+							running = false;
+							break;
+					}
+					break;
+			}
+		}
+	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
+
 	return 0;
 }
